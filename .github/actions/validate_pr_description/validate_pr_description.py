78c1eff877e3d4756a7987518318013b20e9382f
Aleksandr.Pirogov <a.g.pirogov@gmail.com>
2025-02-09 22:08:50 +0200

    Add GitHub status posting functionality and refactor validation script

--- .github/actions/validate_pr_description/validate_pr_description.py
+++ .github/actions/validate_pr_description/validate_pr_description.py
@@ -1,10 +1,5 @@
 import sys
 import re
-import datetime
-import os
-import json
-from github import Github, Auth as GithubAuth
-from github.PullRequest import PullRequest
 from typing import Tuple
 
 issue_patterns = [
@@ -100,48 +95,6 @@ def validate_pr_description_from_file(file_path) -> Tuple[bool, str]:
         txt = f"Failed to validate PR description: {e}"
         print(f"::error::{txt}")
         return False, txt
-    
-def post_status_to_github(is_valid, error_description):
-    gh = Github(auth=GithubAuth.Token(os.environ["GITHUB_TOKEN"]))
-
-    with open(os.environ["GITHUB_EVENT_PATH"]) as fp:
-        event = json.load(fp)
-
-    pr = gh.create_from_raw_data(PullRequest, event["pull_request"])
-
-    header = f"<!-- status pr={pr.number}, validate PR description status -->"
-
-    body = comment = None
-    for c in pr.get_issue_comments():
-        if c.body.startswith(header):
-            print(f"found comment id={c.id}")
-            comment = c
-            body = [c.body]
-
-    if body is None:
-        body = [header]
-
-    status_to_header = {
-        True: "Validation successful",
-        False: "Validation failed"
-    }
-
-    color = "green" if is_valid else "red"
-    indicator = f":{color}_circle:"
-    timestamp_str = datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d %H:%M:%S UTC")
-    body.append(f"{indicator} `{timestamp_str}` {status_to_header[is_valid]}")
-
-    if not is_valid:
-        body += f"\n{error_description}"
-
-    body = "\n".join(body)
-
-    if comment:
-        print(f"edit comment")
-        comment.edit(body)
-    else:
-        print(f"post new comment")
-        pr.create_issue_comment(body)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -150,6 +103,7 @@ if __name__ == "__main__":
     
     file_path = sys.argv[1]
     is_valid, txt = validate_pr_description_from_file(file_path)
-    post_status_to_github(is_valid, txt)
+    from post_status_to_github import post
+    post(is_valid, txt)
     if not is_valid:
         sys.exit(1)
