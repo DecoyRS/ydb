b87247b89fe8a819b16ff53606cd62c24a2a58f5
Alexander Pirogov <alexander.pirogov@jetbrains.com>
2025-04-07 21:28:17 +0300

    Some more logs

--- .github/actions/update_changelog/update_changelog.py
+++ .github/actions/update_changelog/update_changelog.py
@@ -184,7 +184,8 @@ if __name__ == "__main__":
             pr_ids = json.load(file)
     except Exception as e:
         print(f"::error::Failed to read or parse PR data file: {e}")
-        print(f"::error::Contents of the file: {file.read()}")
+        with open(pr_data_file, 'r') as file:
+            print(f"::error::Contents of the file: {file.read()}")
         sys.exit(1)
 
     pr_data = []
